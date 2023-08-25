745b4a7371818758ed298f0d43eb04056f5c1417
ozod07 <xolmurodovozodbek96@gmail.com>
2023-08-25 12:42:36 +0500

    Ozod was finished

---
+++ script.js
@@ -0,0 +1,72 @@
+//1
+// scope turlari
+// 3ga bo'linadi bular:Function, Global,Blog;
+// Functionga funksiya kiradi
+//Blog If,For, While, Do while kiradi 
+//Global let kiradi
+
+
+//2
+// function submultiples(){
+//     let d=7;
+//     for(let i=1;i<=d;i++){
+//         if(d%i==0){
+//             console.log(i);
+//         }
+//     }
+// }
+// submultiples();
+
+
+
+
+//3
+// function grade(){
+//     let k=3;
+//     switch(k){
+//        case 1:
+//         console.log("1-yomon") 
+//         break;
+//         case 2:
+//         console.log("2-qoniqarsiz") 
+//         break;
+//         case 3:
+//         console.log("3-qoniqarli") 
+//         break;
+//         case 4:
+//         console.log("4-yaxshi") 
+//         break;
+//         case 5:
+//         console.log("5-a'lo") 
+//         break;
+//         default:
+//             console.log("Xato")
+        
+//     }
+// }
+// grade();
+
+
+
+
+
+//4
+// function time(){
+//     let d=12321;
+//     let clock=parseInt(d/3600);
+//     let secund=d-clock*3600;
+//     console.log(`Bu kun boshidan boshlab ${clock} soat ${secund} o'tdi`)
+// }
+// time();
+
+
+
+
+//5
+// JavaScript - bu interaktiv va dinamik veb-saytlarni
+// yaratish uchun keng qo'llaniladigan dasturlash tili.
+// U birinchi marta 1995 yilda Brendan Eich tomonidan ishlab
+// chiqilgan va hozirda ECMAScript standartlari organi tomonidan
+//  qo'llab-quvvatlanadi.
+// 1995-yil Browserda ishlay boshlaydi
+// 2009- yil Node JS orqali back endga qo'shiladi
